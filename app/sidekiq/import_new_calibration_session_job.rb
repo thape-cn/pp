af0a64ed441f2ca9ee06cdfde5a1600334e543f1
Eric Guo <eric.guocz@gmail.com>
2023-08-23 17:05:46 +0800

    Move to dedicate service class.

--- app/sidekiq/import_new_calibration_session_job.rb
+++ app/sidekiq/import_new_calibration_session_job.rb
@@ -4,6 +4,6 @@ class ImportNewCalibrationSessionJob
   def perform(import_excel_file_id)
     import_excel_file = ImportExcelFile.find(import_excel_file_id)
     Rails.logger.info "Start importing new evaluation job, import_excel_file_id: #{import_excel_file_id}"
-    Initiation.new_calibration(import_excel_file.company_evaluation, import_excel_file.excel_file)
+    InitiationNewCalibration.do_import(import_excel_file.company_evaluation, import_excel_file.excel_file)
   end
 end
