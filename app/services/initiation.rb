4bb0ba6293b6495e853a61b9dfb68546265b2d1d
Eric Guo <eric.guocz@gmail.com>
2023-08-23 17:09:31 +0800

    Move new_evaluation to dedicate service class.

--- app/services/initiation.rb
+++ app/services/initiation.rb
@@ -28,54 +28,6 @@ class Initiation
     end
   end
 
-  def self.new_evaluation(company_evaluation, excel_file)
-    xlsx = Roo::Excelx.new(StringIO.new(excel_file.download))
-    xlsx.default_sheet = "启动表单"
-
-    xlsx.each(
-      clerk_code: "USERNAME",
-      dept_code: "CUSTOM01",
-      st_code: "STCODE",
-      template_title: "Template"
-    ) do |h|
-      clerk_code = h[:clerk_code].to_s
-      next if clerk_code == "USERNAME"
-      next if clerk_code.blank?
-
-      dept_code = h[:dept_code].to_s
-      st_code = h[:st_code].to_s
-      template_title = h[:template_title].to_s
-
-      user = User.find_by(clerk_code: clerk_code)
-      if user.blank?
-        puts "User not found by clerk_code: #{clerk_code}"
-        next
-      end
-      job_role = JobRole.find_by(st_code: st_code)
-      if job_role.blank?
-        puts "Job_role not found by st_code: #{st_code}"
-        next
-      end
-      company_evaluation_template = company_evaluation.company_evaluation_templates.find_by(title: template_title)
-      if company_evaluation_template.blank?
-        puts "Company evaluation template not found by title: #{template_title}"
-        next
-      end
-
-      ujr = UserJobRole.find_by!(user_id: user.id, job_role_id: job_role.id, dept_code: dept_code)
-      evaluation_user_capability = EvaluationUserCapability.new(user_id: user.id, job_role_id: job_role.id, company_evaluation_template_id: company_evaluation_template.id)
-      evaluation_user_capability.manager_user_id = ujr.manager_user_id
-      evaluation_user_capability.company = ujr.company
-      evaluation_user_capability.department = ujr.department
-      evaluation_user_capability.dept_code = ujr.dept_code
-      evaluation_user_capability.title = ujr.title
-      if (success = evaluation_user_capability.save)
-        evaluation_user_capability.initial_capability_score_filling
-      end
-      puts "evaluation_user_capability: #{dept_code} #{clerk_code} #{template_title} failed #{evaluation_user_capability.errors.full_messages.to_sentence}" unless success
-    end
-  end
-
   def self.new_performance(company_evaluation, csv_file_path)
     CSV.foreach(csv_file_path, headers: true) do |row|
       action = row["ACTION"]
