037351b084258c77690a48009ae19067b494a82b
Eric Guo <eric.guocz@gmail.com>
2023-08-23 17:37:46 +0800

    Add missing translation.

--- app/models/import_excel_file.rb
+++ app/models/import_excel_file.rb
@@ -6,7 +6,8 @@ class ImportExcelFile < ApplicationRecord
   def self.import_type_options
     {
       I18n.t("import_exports.import_type.new_evaluation") => "new_evaluation",
-      I18n.t("import_exports.import_type.new_calibration_session") => "new_calibration_session"
+      I18n.t("import_exports.import_type.new_calibration_session") => "new_calibration_session",
+      I18n.t("import_exports.import_type.new_performance") => "new_performance"
     }
   end
 end
