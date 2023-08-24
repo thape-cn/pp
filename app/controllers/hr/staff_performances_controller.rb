1f9c1e4f6cf2473ff87b4ab252ee2d785d5d8976
Eric Guo <eric.guocz@gmail.com>
2023-08-24 17:14:46 +0800

    Do filter by company evaluation.

--- app/controllers/hr/staff_performances_controller.rb
+++ app/controllers/hr/staff_performances_controller.rb
@@ -3,7 +3,12 @@ module HR
     include StaffManagerGroup
 
     def index
+      @company_evaluation_id = params[:company_evaluation_id].presence
+      company_evaluation = CompanyEvaluation.find_by(id: @company_evaluation_id) || CompanyEvaluation.last
+      company_template_ids = CompanyEvaluationTemplate.where(group_level: "staff")
+        .where(company_evaluation_id: company_evaluation.id).pluck(:id)
       evaluation_user_capabilities = policy_scope(EvaluationUserCapability)
+        .where(company_evaluation_template_id: company_template_ids)
       @evaluation_user_capabilities_group = staff_group(evaluation_user_capabilities)
     end
   end
