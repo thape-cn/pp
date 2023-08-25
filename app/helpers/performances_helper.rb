ddae8fccc6c83847817149b2412e988ede73faf0
Eric Guo <eric.guocz@gmail.com>
2023-08-25 11:01:36 +0800

    Just give array is enough.

--- app/helpers/performances_helper.rb
+++ app/helpers/performances_helper.rb
@@ -1,6 +1,8 @@
 module PerformancesHelper
-  def display_evaluation_user_capabilities(key)
-    @evaluation_user_capabilities_group[key]&.take(9)&.each do |euc|
+  def display_evaluation_user_capabilities(evaluation_user_capabilities)
+    return nil unless evaluation_user_capabilities.present?
+
+    evaluation_user_capabilities.take(9).each do |euc|
       concat(content_tag(:span, class: "btn btn-light rounded-pill m-1") do
         concat(euc.user.chinese_name)
         concat(content_tag(:span, euc.total_evaluation_score, class: "badge ms-1 rounded-pill bg-info"))
