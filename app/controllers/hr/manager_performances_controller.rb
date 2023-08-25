8cfa5e6dc8ef6100424c5a81ee471ec112a62fde
Eric Guo <eric.guocz@gmail.com>
2023-08-25 09:41:33 +0800

    Do filter by department evaluation.

--- app/controllers/hr/manager_performances_controller.rb
+++ app/controllers/hr/manager_performances_controller.rb
@@ -4,14 +4,17 @@ module HR
 
     def index
       @company_evaluation_id = params[:company_evaluation_id].presence
-      @company = params[:company].presence
+      @department = params[:department].presence
       company_evaluation = CompanyEvaluation.find_by(id: @company_evaluation_id) || CompanyEvaluation.last
       company_template_ids = CompanyEvaluationTemplate.where(group_level: "manager")
         .where(company_evaluation_id: company_evaluation.id).pluck(:id)
       evaluation_user_capabilities = policy_scope(EvaluationUserCapability)
         .where(company_evaluation_template_id: company_template_ids)
       @companies = evaluation_user_capabilities.select(:company).distinct.pluck(:company)
-      evaluation_user_capabilities = evaluation_user_capabilities.where(company: @company) if @company.present?
+      @company = params[:company].presence || @companies.first
+      evaluation_user_capabilities = evaluation_user_capabilities.where(company: @company)
+      @departments = evaluation_user_capabilities.select(:department).distinct.pluck(:department)
+      evaluation_user_capabilities = evaluation_user_capabilities.where(department: @department) if @department.present?
       @evaluation_user_capabilities_group = manager_group(evaluation_user_capabilities)
     end
   end
