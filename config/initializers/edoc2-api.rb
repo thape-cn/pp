6804d24f90c6d661c9f8af183181749ba5ea667e
Eric Guo <eric.guocz@gmail.com>
2023-09-08 17:20:42 +0800

    Using value from credentials

--- config/initializers/edoc2-api.rb
+++ config/initializers/edoc2-api.rb
@@ -1,4 +1,4 @@
-Edoc2.loginName = "loginName"
-Edoc2.ipAddress = "ipAddress"
-Edoc2.integrationKey = "integrationKey"
-Edoc2.base_url = "https://base_url" # notice no / at end
+Edoc2.loginName = Rails.application.credentials[Rails.env.to_sym][:edoc2_login_name]
+Edoc2.ipAddress = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address || Socket.ip_address_list.detect(&:ipv4?)&.ip_address
+Edoc2.integrationKey = Rails.application.credentials[Rails.env.to_sym][:edoc2_integration_key]
+Edoc2.base_url = Rails.application.credentials[Rails.env.to_sym][:edoc2_base_url] # notice no / at end
