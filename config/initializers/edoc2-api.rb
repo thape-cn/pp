8a11816db529c400ab351e5cb1f27c98be0d18ce
Eric-Guo <eric.guocz@gmail.com>
2023-09-09 16:05:08 +0800

    Fix test.

--- config/initializers/edoc2-api.rb
+++ config/initializers/edoc2-api.rb
@@ -1,3 +1,4 @@
+return if Rails.env.test?
 Edoc2.loginName = Rails.application.credentials[Rails.env.to_sym][:edoc2_login_name]
 Edoc2.ipAddress = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address || Socket.ip_address_list.detect(&:ipv4?)&.ip_address
 Edoc2.integrationKey = Rails.application.credentials[Rails.env.to_sym][:edoc2_integration_key]
