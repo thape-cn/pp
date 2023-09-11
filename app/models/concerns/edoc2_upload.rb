80ee56fc47061758aa268bdd9dc82bf2ee8fb72c
Eric Guo <eric.guocz@gmail.com>
2023-09-11 16:43:35 +0800

    Do the upload_meta_arch

--- app/models/concerns/edoc2_upload.rb
+++ app/models/concerns/edoc2_upload.rb
@@ -22,5 +22,53 @@ module Edoc2Upload
       res = Edoc2.collect_archives_enclosure_file(pdf_path, edoc_guid)
       update(edoc_file_id: res[0]["fileId"])
     end
+
+    def upload_meta_arch
+      return unless edoc_file_id.present? && edoc_guid.present?
+
+      res = Edoc2.get_dossier_and_arch_type_id_list(user.clerk_code)
+
+      if res.length > 0
+        dossier_id = res[0]["dossierId"]
+        arch_type_id = res[0]["archTypeId"]
+
+        file_info_hash = {
+          Id: edoc_guid,
+          entrystate: "1", # 档案库
+          archtypeid: arch_type_id,
+          name: company_evaluation_template.title,
+          pnName: user.chinese_name,
+          number: "",
+          carrier: "电子",
+          objtype: "",
+          entitynum: "",
+          writtendate: Time.zone.now.strftime("%Y-%m-%d"),
+          duration: "永久",
+          secert: "普通商密",
+          ifInbound: "0",
+          storageroomId: "",
+          ifDossiered: "1", # 是卷内文件
+          dossierId: dossier_id,
+          dataSources: "HR系统",
+          agencyItemNumber: "1",
+          totalNumberPages: "",
+          achievePeople: "PP系统",
+          contractType: "",
+          contractValidity: "",
+          interviewDate: "",
+          assessmentClassification: "",
+          evaluationDate: sign_date&.strftime("%Y-%m-%d") || "",
+          pnCode: user.clerk_code,
+          fileList: [{
+            fileId: edoc_file_id.to_i,
+            operateType: "upload"
+          }]
+        }
+        Rails.logger.info "file_info_hash: #{file_info_hash}"
+        meta_arch_return_hash = Edoc2.meta_arch(file_info_hash)
+        Rails.logger.info "meta_arch_return_hash: #{meta_arch_return_hash}"
+        meta_arch_return_hash
+      end
+    end
   end
 end
