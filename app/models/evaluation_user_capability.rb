f352fe3bb0c5e3b7ccdd64e55f05ad8f81173a09
Eric Guo <eric.guocz@gmail.com>
2023-09-11 14:48:51 +0800

    Now able to upload to edoc files and record fileID.

--- app/models/evaluation_user_capability.rb
+++ app/models/evaluation_user_capability.rb
@@ -147,6 +147,15 @@ class EvaluationUserCapability < ApplicationRecord
     update(edoc_guid: guid)
   end
 
+  def upload_pdf_to_edoc
+    return if edoc_guid.blank?
+    return if edoc_file_id.present?
+
+    pdf_path = Rails.root.join("edoc_guid_pdf/#{edoc_guid}.pdf")
+    res = Edoc2.collect_archives_enclosure_file(pdf_path, edoc_guid)
+    update(edoc_file_id: res[0]["fileId"])
+  end
+
   def self.form_status_options
     {
       I18n.t("evaluation.form_status.initial") => "initial",
