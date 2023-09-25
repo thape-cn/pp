a0f04ce6f8a8dc9014def1e2bbcc5004f01a7780
Eric Guo <eric.guocz@gmail.com>
2023-09-25 15:29:56 +0800

    Another allow blank case.

--- config/initializers/edoc2-api.rb
+++ config/initializers/edoc2-api.rb
@@ -1,5 +1,5 @@
 return if Rails.env.test?
-Edoc2.loginName = Rails.application.credentials[Rails.env.to_sym][:edoc2_login_name]
+Edoc2.loginName = Rails.application.credentials.dig(Rails.env.to_sym, :edoc2_login_name)
 Edoc2.ipAddress = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address || Socket.ip_address_list.detect(&:ipv4?)&.ip_address
-Edoc2.integrationKey = Rails.application.credentials[Rails.env.to_sym][:edoc2_integration_key]
-Edoc2.base_url = Rails.application.credentials[Rails.env.to_sym][:edoc2_base_url] # notice no / at end
+Edoc2.integrationKey = Rails.application.credentials.dig(Rails.env.to_sym, :edoc2_integration_key)
+Edoc2.base_url = Rails.application.credentials.dig(Rails.env.to_sym, :edoc2_base_url) # notice no / (slash) at end
