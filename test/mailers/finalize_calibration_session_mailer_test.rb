76738a3be9b4f3ba39920b2ebcef663e71df40f8
Eric Guo <eric.guocz@gmail.com>
2023-10-07 16:31:25 +0800

    Add dummy user PP Test 0 ~ 9

--- test/mailers/finalize_calibration_session_mailer_test.rb
+++ test/mailers/finalize_calibration_session_mailer_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class FinalizeCalibrationSessionMailerTest < ActionMailer::TestCase
   test "notification_manager" do
-    mail = FinalizeCalibrationSessionMailer.with(manager: User.first, calibration_session: CalibrationSession.first).notification_manager
+    mail = FinalizeCalibrationSessionMailer.with(manager: User.find_by(email: "guochunzhong@thape.com.cn"), calibration_session: CalibrationSession.first).notification_manager
     assert_equal "【绩效评价通知】您的下属绩效等级已定案", mail.subject
     assert_equal ["guochunzhong@thape.com.cn"], mail.to
     assert_equal ["performance@thape.com.cn"], mail.from
