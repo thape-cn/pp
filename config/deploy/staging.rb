f26c70ce2c919d23077f315e9779b2f861a55f3e
Eric Guo <eric.guocz@gmail.com>
2023-10-08 16:20:32 +0800

    Add sidekiq settings.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,5 +5,7 @@ set :branch, :main
 set :rails_env, "production"
 set :puma_service_unit_name, :puma_rails_starter
 set :puma_systemctl_user, :system
+set :sidekiq_service_unit_name, "sidekiq_pp_vendor"
+set :sidekiq_service_unit_user, :system
 
 server "bandwagon", user: "starter", roles: %w[app db web]
