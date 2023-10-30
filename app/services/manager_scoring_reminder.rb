a7f540acd9145b872bcc8bde02bc093db8f1b5a9
Eric Guo <eric.guocz@gmail.com>
2023-10-30 15:41:20 +0800

    Scope is all current company evaluation.

--- app/services/manager_scoring_reminder.rb
+++ app/services/manager_scoring_reminder.rb
@@ -2,9 +2,10 @@
 
 class ManagerScoringReminder
   def self.need_sending_wechat_message?(evaluation_user_capability)
-    current_company_evaluation_template_id = evaluation_user_capability.company_evaluation_template_id
+    current_company_evaluation = evaluation_user_capability.company_evaluation_template.company_evaluation
     current_manager_user_id = evaluation_user_capability.manager_user_id
-    all_reporting_eucs = EvaluationUserCapability.where(company_evaluation_template_id: current_company_evaluation_template_id, manager_user_id: current_manager_user_id)
+    all_reporting_eucs = EvaluationUserCapability.where(manager_user_id: current_manager_user_id,
+      company_evaluation_template_id: current_company_evaluation.company_evaluation_templates.pluck(:id))
     still_need_self_assessment_eucs_remainding = all_reporting_eucs.any? { |euc| euc.form_status == "initial" }
     still_need_self_assessment_eucs_remainding ? false : true
   end
