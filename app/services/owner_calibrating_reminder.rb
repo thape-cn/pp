08be30d3f4eeb2059965ec1fedf51ac963148449
Eric Guo <eric.guocz@gmail.com>
2023-10-31 16:25:06 +0800

    Check users all owned calibration session and try to sent once only.

--- app/services/owner_calibrating_reminder.rb
+++ app/services/owner_calibrating_reminder.rb
@@ -6,19 +6,28 @@ class OwnerCalibratingReminder
   end
 
   def collect_need_sending_calibrating_remind(calibration_session)
-    all_evaluation_user_capabilities = calibration_session.calibration_session_users.collect(&:evaluation_user_capability)
-    no_any_new_calibration_session = !calibration_session.calibration_session_users.any? { |csu| csu.new_calibration_session_id.present? }
-    eucs_all_in_manager_scored = all_evaluation_user_capabilities.all? { |euc| euc.form_status == "manager_scored" }
-    if no_any_new_calibration_session && eucs_all_in_manager_scored
+    if calibration_session.can_sending_calibrating_remind?
       @need_sending_calibration_sessions << calibration_session
     end
   end
 
   def send_calibrating_remind_messages
-    @need_sending_calibration_sessions.each do |calibration_session|
-      owner_user = calibration_session.owner
+    need_sending_calibration_owners.each do |owner_user|
       wecom_id = owner_user.wecom_id.present? ? owner_user.wecom_id : owner_user.email.split("@")[0]
       OwnerNeedCalibratingRemindJob.perform_async(wecom_id)
     end
+  end
+
+  private
+
+  def need_sending_calibration_owners
+    need_check_owners = @need_sending_calibration_sessions.compact.collect(&:owner).compact
+    need_sending_calibration_owners = []
+    need_check_owners.each do |owner|
+      if owner.owned_calibration_sessions.all? { |cs| cs.can_sending_calibrating_remind? }
+        need_sending_calibration_owners << owner
+      end
+    end
+    need_sending_calibration_owners.compact
   end
 end
