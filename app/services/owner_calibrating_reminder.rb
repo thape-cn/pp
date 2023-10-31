264d5638ff0725b0a98f3758a8968f747830bbb7
Eric Guo <eric.guocz@gmail.com>
2023-10-31 15:17:53 +0800

    Logic of the need sending calibrating remind

--- app/services/owner_calibrating_reminder.rb
+++ app/services/owner_calibrating_reminder.rb
@@ -6,6 +6,12 @@ class OwnerCalibratingReminder
   end
 
   def collect_need_sending_calibrating_remind(calibration_session)
+    all_evaluation_user_capabilities = calibration_session.calibration_session_users.collect(&:evaluation_user_capability)
+    no_any_new_calibration_session = !calibration_session.calibration_session_users.any? { |csu| csu.new_calibration_session_id.present? }
+    eucs_all_in_manager_scored = all_evaluation_user_capabilities.all? { |euc| euc.form_status == "manager_scored" }
+    if no_any_new_calibration_session && eucs_all_in_manager_scored
+      @need_sending_calibration_sessions << calibration_session
+    end
   end
 
   def send_calibrating_remind_messages
