f44c0fde382d3ed973bfd43b90dbf9bd552aff92
过纯中 <guochunzhong@thape.com.cn>
2023-11-01 10:01:34 +0800

    word change, no logic change.

--- app/sidekiq/owner_need_calibrating_remind_job.rb
+++ app/sidekiq/owner_need_calibrating_remind_job.rb
@@ -4,7 +4,7 @@ class OwnerNeedCalibratingRemindJob
   def perform(wecom_id)
     sent_message = <<~HELLO
       【绩效评价通知】
-      您的经理均已完成评分，请尽快登录绩效系统进行校准操作。
+      校准会话已激活，请尽快登录绩效系统进行操作。
       登录链接：https://performance.thape.com.cn/
     HELLO
 
