41695580eb47cd47eea9f59956562e4e64c5d2fc
Eric Guo <eric.guocz@gmail.com>
2023-11-07 17:07:35 +0800

    Now will display icon for CapabilitySelectWithDescription

--- app/javascript/react/table/CapabilitySelectWithDescription.jsx
+++ app/javascript/react/table/CapabilitySelectWithDescription.jsx
@@ -1,10 +1,18 @@
 import * as React from "react";
+import {svgInfo} from "../utils/tableHeader";
 
 export function CapabilitySelectWithDescription({ value, ercd, onChange, onBlur, matric, not_rated_text }) {
-  return <select value={value} onChange={onChange} onBlur={onBlur} className="form-control form-control-sm">
-    <option value='0'>{not_rated_text}</option>
-    {matric.map(option => (
-      <option key={option.value} value={option.value}>{option.label}</option>
-    ))}
-  </select>
+  return <div className="input-group">
+    <select value={value} onChange={onChange} onBlur={onBlur} className="form-control form-control-sm">
+      <option value='0'>{not_rated_text}</option>
+      {matric.map(option => (
+        <option key={option.value} value={option.value}>{option.label}</option>
+      ))}
+    </select>
+    <span className="input-group-text" role="button">
+      <svg className="icon">
+        <use xlinkHref={svgInfo()}></use>
+      </svg>
+    </span>
+  </div>
 }
