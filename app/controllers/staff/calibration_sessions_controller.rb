21e43ec2ec2974062d7347437683841cb87f64ef
Eric Guo <eric.guocz@gmail.com>
2023-11-13 15:42:42 +0800

    Rename update_staff_by_square to update_person_by_square

--- app/controllers/staff/calibration_sessions_controller.rb
+++ app/controllers/staff/calibration_sessions_controller.rb
@@ -151,15 +151,15 @@ module Staff
     end
 
     def update_staff_group(calibration)
-      update_staff_by_square(calibration["11"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["12"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["13"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["21"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["22"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["23"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["31"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["32"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
-      update_staff_by_square(calibration["33"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["11"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["12"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["13"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["21"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["22"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["23"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["31"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["32"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
+      update_person_by_square(calibration["33"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
     end
 
     def check_enforce_distribute_for_staff_group(calibration)
@@ -195,18 +195,18 @@ module Staff
     end
 
     def update_manager_group(calibration)
-      update_staff_by_square(calibration["11"]) { |euc| euc.update(calibration_management_profession_score: 1, calibration_performance_score: 5) }
-      update_staff_by_square(calibration["12"]) { |euc| euc.update(calibration_management_profession_score: 3, calibration_performance_score: 5) }
-      update_staff_by_square(calibration["13"]) { |euc| euc.update(calibration_management_profession_score: 5, calibration_performance_score: 5) }
-      update_staff_by_square(calibration["21"]) { |euc| euc.update(calibration_management_profession_score: 1, calibration_performance_score: 3) }
-      update_staff_by_square(calibration["22"]) { |euc| euc.update(calibration_management_profession_score: 3, calibration_performance_score: 3) }
-      update_staff_by_square(calibration["23"]) { |euc| euc.update(calibration_management_profession_score: 5, calibration_performance_score: 3) }
-      update_staff_by_square(calibration["31"]) { |euc| euc.update(calibration_management_profession_score: 1, calibration_performance_score: 1) }
-      update_staff_by_square(calibration["32"]) { |euc| euc.update(calibration_management_profession_score: 3, calibration_performance_score: 1) }
-      update_staff_by_square(calibration["33"]) { |euc| euc.update(calibration_management_profession_score: 5, calibration_performance_score: 1) }
+      update_person_by_square(calibration["11"]) { |euc| euc.update(calibration_management_profession_score: 1, calibration_performance_score: 5) }
+      update_person_by_square(calibration["12"]) { |euc| euc.update(calibration_management_profession_score: 3, calibration_performance_score: 5) }
+      update_person_by_square(calibration["13"]) { |euc| euc.update(calibration_management_profession_score: 5, calibration_performance_score: 5) }
+      update_person_by_square(calibration["21"]) { |euc| euc.update(calibration_management_profession_score: 1, calibration_performance_score: 3) }
+      update_person_by_square(calibration["22"]) { |euc| euc.update(calibration_management_profession_score: 3, calibration_performance_score: 3) }
+      update_person_by_square(calibration["23"]) { |euc| euc.update(calibration_management_profession_score: 5, calibration_performance_score: 3) }
+      update_person_by_square(calibration["31"]) { |euc| euc.update(calibration_management_profession_score: 1, calibration_performance_score: 1) }
+      update_person_by_square(calibration["32"]) { |euc| euc.update(calibration_management_profession_score: 3, calibration_performance_score: 1) }
+      update_person_by_square(calibration["33"]) { |euc| euc.update(calibration_management_profession_score: 5, calibration_performance_score: 1) }
     end
 
-    def update_staff_by_square(calibration_square)
+    def update_person_by_square(calibration_square)
       return if calibration_square.nil?
       calibration_square.each do |item|
         euc = EvaluationUserCapability.find item["id"]
