1cbfbce6d290b7c9c39553a56ff941ff3f190951
Eric Guo <eric.guocz@gmail.com>
2023-11-21 11:13:31 +0800

    Extract the CheckEnforceDistributeForGroup as concern.

--- app/controllers/staff/calibration_sessions_controller.rb
+++ app/controllers/staff/calibration_sessions_controller.rb
@@ -3,6 +3,7 @@ module Staff
     include Pagy::Backend
     include StaffManagerGroup
     include UpdateSessionGroup
+    include CheckEnforceDistributeForGroup
     before_action :check_brower, only: %i[show], if: -> { request.format.html? }
     after_action :verify_authorized, except: %i[index expender]
     after_action :verify_policy_scoped, only: :index
@@ -149,54 +150,6 @@ module Staff
         manager_group(need_calibration_evaluation_user_capabilities)
       when "manager"
         manager_group(need_calibration_evaluation_user_capabilities)
-      end
-    end
-
-    def check_enforce_distribute_for_staff_group(calibration)
-      lower_quotas_staff = @calibration_session.lower_quotas_staff
-      ct = @calibration_session.calibration_template
-      apa_grade_rate_people_count = calibration["13"]&.length.to_i + calibration["12"]&.length.to_i + calibration["23"]&.length.to_i
-      b_grade_rate_people_count = calibration["11"]&.length.to_i + calibration["22"]&.length.to_i + calibration["33"]&.length.to_i
-      cd_grade_rate_people_count = calibration["31"]&.length.to_i + calibration["21"]&.length.to_i + calibration["32"]&.length.to_i
-      if lower_quotas_staff[:apa_grade_rate] == apa_grade_rate_people_count \
-        && lower_quotas_staff[:b_grade_rate] == b_grade_rate_people_count \
-        && lower_quotas_staff[:cd_grade_rate] == cd_grade_rate_people_count
-        nil
-      else
-        I18n.t("calibration.enforce_distribute_for_staff_failure", apa_grade_rate: ct.apa_grade_rate,
-          b_grade_rate: ct.b_grade_rate, cd_grade_rate: ct.cd_grade_rate)
-      end
-    end
-
-    def check_enforce_distribute_for_auxiliary_group(calibration)
-      lower_quotas_manager = @calibration_session.lower_quotas_manager
-      ct = @calibration_session.calibration_template
-      below_standard_rate_people_count = calibration["13"]&.length.to_i + calibration["12"]&.length.to_i + calibration["23"]&.length.to_i
-      standards_compliant_rate_people_count = calibration["11"]&.length.to_i + calibration["22"]&.length.to_i + calibration["33"]&.length.to_i
-      beyond_standard_rate_people_count = calibration["31"]&.length.to_i + calibration["21"]&.length.to_i + calibration["32"]&.length.to_i
-      if lower_quotas_manager[:below_standard_rate] == below_standard_rate_people_count \
-        && lower_quotas_manager[:standards_compliant_rate] == standards_compliant_rate_people_count \
-        && lower_quotas_manager[:beyond_standard_rate] == beyond_standard_rate_people_count
-        nil
-      else
-        I18n.t("calibration.enforce_distribute_for_manager_failure", below_standard_rate: ct.below_standard_rate,
-          standards_compliant_rate: ct.standards_compliant_rate, beyond_standard_rate: ct.beyond_standard_rate)
-      end
-    end
-
-    def check_enforce_distribute_for_manager_group(calibration)
-      lower_quotas_manager = @calibration_session.lower_quotas_manager
-      ct = @calibration_session.calibration_template
-      below_standard_rate_people_count = calibration["11"]&.length.to_i + calibration["21"]&.length.to_i + calibration["31"]&.length.to_i
-      standards_compliant_rate_people_count = calibration["12"]&.length.to_i + calibration["22"]&.length.to_i + calibration["32"]&.length.to_i
-      beyond_standard_rate_people_count = calibration["13"]&.length.to_i + calibration["23"]&.length.to_i + calibration["33"]&.length.to_i
-      if lower_quotas_manager[:below_standard_rate] == below_standard_rate_people_count \
-        && lower_quotas_manager[:standards_compliant_rate] == standards_compliant_rate_people_count \
-        && lower_quotas_manager[:beyond_standard_rate] == beyond_standard_rate_people_count
-        nil
-      else
-        I18n.t("calibration.enforce_distribute_for_manager_failure", below_standard_rate: ct.below_standard_rate,
-          standards_compliant_rate: ct.standards_compliant_rate, beyond_standard_rate: ct.beyond_standard_rate)
       end
     end
 
