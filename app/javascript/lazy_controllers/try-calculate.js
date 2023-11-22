6b0a2c36b2628e5440042a40b57df27546071d03
Eric Guo <eric.guocz@gmail.com>
2023-11-22 15:24:07 +0800

    Transfer data to backend server.

--- app/javascript/lazy_controllers/try-calculate.js
+++ app/javascript/lazy_controllers/try-calculate.js
@@ -2,13 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from '@rails/request.js'
 
 Stimulus.register("try-calculate", class extends Controller {
-  static targets = [ "apa", "b", "cd", "below", "standard", "beyond",
+  static targets = [ "peopleCount", "apa", "b", "cd", "below", "standard", "beyond",
    "apaRate", "bRate", "cdRate", "belowRate", "standardRate", "beyondRate"]
   static values = { groupLevel: String }
 
   click() {
     event.preventDefault();
-    get(`/admin/calibration_sessions/calculate.json?group_level=${this.groupLevelValue}`).then((response) => {
+    const url = ['staff', 'auxiliary'].includes(this.groupLevelValue) ?
+      `/admin/calibration_sessions/calculate.json?group_level=${this.groupLevelValue}&people=${this.peopleCountTarget.value}&apa=${this.apaRateTarget.value}&b=${this.bRateTarget.value}&cd=${this.cdRateTarget.value}` :
+      `/admin/calibration_sessions/calculate.json?group_level=${this.groupLevelValue}&people=${this.peopleCountTarget.value}&beyond=${this.beyondRateTarget.value}&standard=${this.standardRateTarget.value}&below=${this.belowRateTarget.value}`;
+
+    get(url).then((response) => {
       if (response.ok) {
         const result_json = response.json;
         result_json.then(result => {
@@ -16,15 +20,5 @@ Stimulus.register("try-calculate", class extends Controller {
         });
       }
     });
-
-    if (['staff', 'auxiliary'].includes(this.groupLevelValue)) {
-      console.log(this.apaRateTarget);
-      console.log(this.bRateTarget);
-      console.log(this.cdRateTarget);
-    } else {
-      console.log(this.beyondRateTarget);
-      console.log(this.standardRateTarget);
-      console.log(this.belowRateTarget);
-    }
   }
 });
