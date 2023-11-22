ae8427e00ad3f3d4b183a0a4204031998118b837
Eric Guo <eric.guocz@gmail.com>
2023-11-22 14:39:42 +0800

    Display target instead of value.

--- app/javascript/lazy_controllers/try-calculate.js
+++ app/javascript/lazy_controllers/try-calculate.js
@@ -2,9 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from '@rails/request.js'
 
 Stimulus.register("try-calculate", class extends Controller {
+  static targets = [ "apa", "b", "cd", "below", "standard", "beyond" ]
   static values = { groupLevel: String }
 
   click() {
-    console.log(this.groupLevelValue);
+    if (['staff', 'auxiliary'].includes(this.groupLevelValue)) {
+      console.log(this.apaTarget);
+      console.log(this.bTarget);
+      console.log(this.cdTarget);
+    } else {
+      console.log(this.belowTarget);
+      console.log(this.standardTarget);
+      console.log(this.beyondTarget);
+    }
   }
 });
