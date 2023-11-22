906905da16a971ed44cc34297d9b976a6dc98b5d
Eric Guo <eric.guocz@gmail.com>
2023-11-22 14:29:44 +0800

    Will display group level when click try calculate.

--- app/javascript/lazy_controllers/try-calculate.js
+++ app/javascript/lazy_controllers/try-calculate.js
@@ -2,12 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from '@rails/request.js'
 
 Stimulus.register("try-calculate", class extends Controller {
-  static targets = [ "icon" ];
-  static values = {
-    groupLevel: String
-  }
+  static values = { groupLevel: String }
 
   click() {
-    console.log("Button clicked");
+    console.log(this.groupLevelValue);
   }
 });
