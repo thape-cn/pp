2b4ce9cfbdbc1e8bfe3ea892c253290d1400421e
Eric-Guo <eric.guocz@gmail.com>
2023-11-24 15:02:08 +0800

    Add real enforce highest only logic in checking.

--- app/controllers/concerns/check_enforce_distribute_for_group.rb
+++ app/controllers/concerns/check_enforce_distribute_for_group.rb
@@ -9,7 +9,14 @@ module CheckEnforceDistributeForGroup
     apa_grade_rate_people_count = calibration["13"]&.length.to_i + calibration["12"]&.length.to_i + calibration["23"]&.length.to_i
     b_grade_rate_people_count = calibration["11"]&.length.to_i + calibration["22"]&.length.to_i + calibration["33"]&.length.to_i
     cd_grade_rate_people_count = calibration["31"]&.length.to_i + calibration["21"]&.length.to_i + calibration["32"]&.length.to_i
-    if lower_quotas_staff[:apa_grade_rate] == apa_grade_rate_people_count \
+
+    if enforce_highest_only
+      if lower_quotas_staff[:apa_grade_rate] >= apa_grade_rate_people_count
+        nil
+      else
+        I18n.t("calibration.enforce_highest_only_for_staff_failure", apa_grade_rate: ct.apa_grade_rate)
+      end
+    elsif lower_quotas_staff[:apa_grade_rate] == apa_grade_rate_people_count \
       && lower_quotas_staff[:b_grade_rate] == b_grade_rate_people_count \
       && lower_quotas_staff[:cd_grade_rate] == cd_grade_rate_people_count
       nil
@@ -25,7 +32,14 @@ module CheckEnforceDistributeForGroup
     below_standard_rate_people_count = calibration["11"]&.length.to_i + calibration["21"]&.length.to_i + calibration["31"]&.length.to_i
     standards_compliant_rate_people_count = calibration["12"]&.length.to_i + calibration["22"]&.length.to_i + calibration["32"]&.length.to_i
     beyond_standard_rate_people_count = calibration["13"]&.length.to_i + calibration["23"]&.length.to_i + calibration["33"]&.length.to_i
-    if lower_quotas_manager[:below_standard_rate] == below_standard_rate_people_count \
+
+    if enforce_highest_only
+      if lower_quotas_manager[:beyond_standard_rate] >= beyond_standard_rate_people_count
+        nil
+      else
+        I18n.t("calibration.enforce_highest_only_for_manager_failure", beyond_standard_rate: ct.beyond_standard_rate)
+      end
+    elsif lower_quotas_manager[:below_standard_rate] == below_standard_rate_people_count \
       && lower_quotas_manager[:standards_compliant_rate] == standards_compliant_rate_people_count \
       && lower_quotas_manager[:beyond_standard_rate] == beyond_standard_rate_people_count
       nil
