ea603a789830c382fc5d574316c99ca799fcc93e
Eric Guo <eric.guocz@gmail.com>
2023-12-08 16:33:45 +0800

    Fix style.

--- app/sidekiq/archive_company_evaluation_role_capability_job.rb
+++ app/sidekiq/archive_company_evaluation_role_capability_job.rb
@@ -3,7 +3,7 @@ class ArchiveCompanyEvaluationRoleCapabilityJob
 
   def perform(company_evaluation_id)
     EvaluationRoleCapability.all.each do |erc|
-      cerc_description = erc.erc_description.presence || erc.capability.description.presence || ''
+      cerc_description = erc.erc_description.presence || erc.capability.description.presence || ""
       EndedCompanyEvaluationRoleCapability.create(company_evaluation_id: company_evaluation_id,
         evaluation_role_id: erc.evaluation_role_id,
         capability_id: erc.capability_id,
