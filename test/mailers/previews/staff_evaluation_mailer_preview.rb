5bd11fe249667055d2015fe7a0516f2e413be86a
Eric Guo <eric.guocz@gmail.com>
2023-12-29 13:46:34 +0800

    Review evaluation mailer again.

--- test/mailers/previews/staff_evaluation_mailer_preview.rb
+++ test/mailers/previews/staff_evaluation_mailer_preview.rb
@@ -2,6 +2,6 @@
 class StaffEvaluationMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/staff_evaluation_mailer/start
   def start
-    StaffEvaluationMailer.with(evaluation_user_capability: EvaluationUserCapability.find(42)).start
+    StaffEvaluationMailer.with(evaluation_user_capability: EvaluationUserCapability.first).start
   end
 end
