adab652e9b0d403d6e46872ecf1cb734c01234ac
Eric Guo <eric.guocz@gmail.com>
2024-01-18 17:04:40 +0800

    Only my name in the calibration

--- app/controllers/admin/hr_homes_controller.rb
+++ app/controllers/admin/hr_homes_controller.rb
@@ -11,13 +11,17 @@ module Admin
       @need_review_evaluations = current_open_evaluations
         .where(manager_user_id: current_user.id)
         .where(form_status: "self_assessment_done")
-      @need_calibration_sessions = policy_scope(CalibrationSession)
+
+      my_name_in_calibration_sessions = CalibrationSession.where(owner_id: current_user.id).left_joins(:calibration_session_judges)
+        .or(CalibrationSession.left_joins(:calibration_session_judges).where(calibration_session_judges: {judge_id: current_user.id}))
+
+      @need_calibration_sessions = my_name_in_calibration_sessions
         .where(session_status: "calibrating")
         .where(calibration_template_id: CalibrationTemplate.open_for_user_calibration_template_ids)
         .distinct
       @need_signing_evaluations = current_open_evaluations
         .where(user_id: current_user.id, form_status: "hr_review_completed")
-      @proofreading_calibration_sessions = policy_scope(CalibrationSession)
+      @proofreading_calibration_sessions = my_name_in_calibration_sessions
         .where(session_status: "proofreading")
         .where(calibration_template_id: CalibrationTemplate.open_for_user_calibration_template_ids)
         .distinct
