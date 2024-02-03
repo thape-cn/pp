c8dcee108f373efd30fa52e88f5c874a05d8e397
Eric Guo <eric.guocz@gmail.com>
2024-02-03 11:24:21 +0800

    Fix preview for FinalizeCalibrationSessionMailer
    http://localhost:3000/rails/mailers/finalize_calibration_session_mailer/notification_manager

--- test/mailers/previews/finalize_calibration_session_mailer_preview.rb
+++ test/mailers/previews/finalize_calibration_session_mailer_preview.rb
@@ -2,6 +2,6 @@
 class FinalizeCalibrationSessionMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/finalize_calibration_session_mailer/notification_manager
   def notification_manager
-    FinalizeCalibrationSessionMailer.with(manager: User.find(771), calibration_session: CalibrationSession.first).notification_manager
+    FinalizeCalibrationSessionMailer.with(manager: User.first, calibration_session: CalibrationSession.first).notification_manager
   end
 end
