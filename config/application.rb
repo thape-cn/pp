cbb1f98512c07caf919c0192695fcd56fc34cd15
Eric Guo <eric.guocz@gmail.com>
2024-06-03 14:15:17 +0800

    Review new Rails 7.2 settings.

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,11 @@ require_relative "../lib/gitlab/request_forgery_protection"
 module CoreuiProRailsStarter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
-    # Please, see https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#config-autoload-lib-ignore.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
