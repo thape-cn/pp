15ee690fab5a1915280445c60b262a56c1f3f68b
Eric Guo <eric.guocz@gmail.com>
2024-06-03 12:14:32 +0800

    Fix DB typo for matric

---
+++ db/migrate/20240603041023_fix_existing_company_evaluation_templates_value.rb
@@ -0,0 +1,10 @@
+class FixExistingCompanyEvaluationTemplatesValue < ActiveRecord::Migration[7.1]
+  def change
+    CompanyEvaluationTemplate.where(work_quality_metric: 'grading_5_matric').update_all(work_quality_metric: 'grading_5_metric')
+    CompanyEvaluationTemplate.where(work_load_metric: 'grading_5_matric').update_all(work_load_metric: 'grading_5_metric')
+    CompanyEvaluationTemplate.where(work_attitude_metric: 'grading_5_matric').update_all(work_attitude_metric: 'grading_5_metric')
+    CompanyEvaluationTemplate.where(professional_management_metric: 'grading_5_matric').update_all(professional_management_metric: 'grading_5_metric')
+    CompanyEvaluationTemplate.where(performance_metric: 'grading_5_matric').update_all(performance_metric: 'grading_5_metric')
+    CompanyEvaluationTemplate.where(total_reverse_metric: 'reverse_5_matric').update_all(total_reverse_metric: 'reverse_5_metric')
+  end
+end
