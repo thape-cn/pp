03ad9deb6a8525ae2424fbb3202fc6c3b009703b
Eric Guo <eric.guocz@gmail.com>
2024-06-06 12:06:51 +0800

    Enable user click and start scoring

--- app/controllers/staff/read_manager_instructions_controller.rb
+++ app/controllers/staff/read_manager_instructions_controller.rb
@@ -2,5 +2,11 @@ module Staff
   class ReadManagerInstructionsController < BaseController
     def show
     end
+
+    def create
+      current_user.update(read_manager_instruction: true)
+      company_evaluation_ids = CompanyEvaluation.open_for_user.pluck(:id)
+      redirect_to staff_mark_score_path(id: current_user.id, company_evaluation_ids: company_evaluation_ids), notice: t(".success")
+    end
   end
 end
