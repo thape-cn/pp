f8079d6327f024ee4be7677ae959c9e4fc4c63be
Eric Guo <eric.guocz@gmail.com>
2024-06-20 13:51:52 +0800

    Add Auxiliary performance result for CP.

---
+++ app/controllers/cp/auxiliary_performances_controller.rb
@@ -0,0 +1,15 @@
+module CP
+  class AuxiliaryPerformancesController < BaseController
+    include ShowPerformance
+
+    private
+
+    def group_level
+      "auxiliary"
+    end
+
+    def group(evaluation_user_capabilities)
+      manager_group(evaluation_user_capabilities)
+    end
+  end
+end
