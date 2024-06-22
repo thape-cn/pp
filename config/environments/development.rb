5de88e3047dbc45ed55b431d21fc45956c96d3e1
Eric Guo <eric.guocz@gmail.com>
2024-06-22 14:59:23 +0800

    Enable deprecation raise.
    https://blog.arkency.com/do-you-tune-out-ruby-deprecation-warnings/

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  config.active_support.deprecation = :raise
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
