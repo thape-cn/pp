a8f490ad408ae100f0d0984e63b05864c96da6ee
Eric Guo <eric.guocz@gmail.com>
2024-07-04 15:39:45 +0800

    Secretary should see the evaluation_progress same as HRBP.

--- app/controllers/staff/evaluation_progresses_controller.rb
+++ app/controllers/staff/evaluation_progresses_controller.rb
@@ -5,15 +5,17 @@ module Staff
     helper_method :pagy
 
     def show
-      if current_user.hrbp_user_managed_departments.present?
+      if current_user.hrbp_user_managed_departments.present? || current_user.secretary_managed_departments.present?
         @display_label = params[:label].presence
         @display_form_status = EvaluationUserCapability.form_status_options[@display_label]
         @company_evaluations = policy_scope(CompanyEvaluation).open_for_user
         @company_evaluation_ids = @company_evaluations.pluck(:id)
+        dept_codes = current_user.hrbp_user_managed_departments.collect(&:managed_dept_code)
+        dept_codes = current_user.secretary_managed_departments.collect(&:managed_dept_code) if dept_codes.blank?
         @evaluation_user_capabilities = EvaluationUserCapability
           .includes(:company_evaluation_template, :user, :manager_user)
           .where(company_evaluation_template: {company_evaluation_id: @company_evaluation_ids})
-          .where(dept_code: current_user.hrbp_user_managed_departments.collect(&:managed_dept_code))
+          .where(dept_code: dept_codes)
 
         respond_to do |format|
           format.html { render }
