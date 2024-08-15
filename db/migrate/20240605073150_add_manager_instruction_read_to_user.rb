53e506174f735d25642fe9faa6630532342be64c
Eric Guo <eric.guocz@gmail.com>
2024-08-15 16:47:34 +0800

    Return to 7.1 first.

--- db/migrate/20240605073150_add_manager_instruction_read_to_user.rb
+++ db/migrate/20240605073150_add_manager_instruction_read_to_user.rb
@@ -1,4 +1,4 @@
-class AddManagerInstructionReadToUser < ActiveRecord::Migration[7.2]
+class AddManagerInstructionReadToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :read_manager_instruction, :boolean, default: false, null: false
   end
