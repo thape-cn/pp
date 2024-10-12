0351c5ced44143618988d12370595bc61f4726f3
Eric Guo <eric.guocz@gmail.com>
2024-10-12 17:09:04 +0800

    Allow search HRBP history eucs

--- app/controllers/staff/history_user_capabilities_controller.rb
+++ app/controllers/staff/history_user_capabilities_controller.rb
@@ -8,6 +8,8 @@ module Staff
     def index
       @company_evaluation = CompanyEvaluation.find params[:company_evaluation_id]
       title = t(".breadcrumb_title", title: @company_evaluation.title)
+      @user_id = params[:user_id]
+      @manager_user_id = params[:manager_user_id]
       add_to_breadcrumbs title, staff_company_evaluation_history_user_capabilities_path(company_evaluation_id: @company_evaluation.id)
       set_meta_tags(title: title)
       evaluation_user_capabilities = policy_scope(EvaluationUserCapability)
@@ -16,6 +18,12 @@ module Staff
         .where(company_evaluation_template: {company_evaluation_id: @company_evaluation.id})
       if current_user.secretary_managed_departments.present?
         evaluation_user_capabilities = evaluation_user_capabilities.where(user_id: current_user.id)
+      end
+      if @user_id.present?
+        evaluation_user_capabilities = evaluation_user_capabilities.where(user_id: @user_id)
+      end
+      if @manager_user_id.present?
+        evaluation_user_capabilities = evaluation_user_capabilities.where(manager_user_id: @manager_user_id)
       end
       @pagy, @evaluation_user_capabilities = pagy(evaluation_user_capabilities, items: current_user.preferred_page_length)
     end
