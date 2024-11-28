5abb7d3ea12e17f6d96c748faf4fa9bea6b5cc31
Eric Guo <eric.guocz@gmail.com>
2024-11-28 21:57:31 +0800

    Swap calibration_work_quality and calibration_work_load, so keep it same as manager_group_a and manager_group_b

--- app/controllers/concerns/update_session_group.rb
+++ app/controllers/concerns/update_session_group.rb
@@ -4,15 +4,15 @@ module UpdateSessionGroup
   private
 
   def update_staff_group(calibration)
-    update_person_by_square(calibration["11"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["12"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["13"]) { |euc| euc.update(calibration_work_load: 5, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["21"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["22"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["23"]) { |euc| euc.update(calibration_work_load: 3, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["31"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 1, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["32"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 3, calibration_work_attitude: euc.work_attitude) }
-    update_person_by_square(calibration["33"]) { |euc| euc.update(calibration_work_load: 1, calibration_work_quality: 5, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["11"]) { |euc| euc.update(calibration_work_quality: 1, calibration_work_load: 5, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["12"]) { |euc| euc.update(calibration_work_quality: 3, calibration_work_load: 5, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["13"]) { |euc| euc.update(calibration_work_quality: 5, calibration_work_load: 5, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["21"]) { |euc| euc.update(calibration_work_quality: 1, calibration_work_load: 3, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["22"]) { |euc| euc.update(calibration_work_quality: 3, calibration_work_load: 3, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["23"]) { |euc| euc.update(calibration_work_quality: 5, calibration_work_load: 3, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["31"]) { |euc| euc.update(calibration_work_quality: 1, calibration_work_load: 1, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["32"]) { |euc| euc.update(calibration_work_quality: 3, calibration_work_load: 1, calibration_work_attitude: euc.work_attitude) }
+    update_person_by_square(calibration["33"]) { |euc| euc.update(calibration_work_quality: 5, calibration_work_load: 1, calibration_work_attitude: euc.work_attitude) }
   end
 
   def update_manager_group_a(calibration)
