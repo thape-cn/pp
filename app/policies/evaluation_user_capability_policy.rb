62970cc92814f99fbe0c3b2098814fe8386fa6c8
Eric Guo <eric.guocz@gmail.com>
2024-12-25 16:19:41 +0800

    HRBP can be owner of calibration.

--- app/policies/evaluation_user_capability_policy.rb
+++ app/policies/evaluation_user_capability_policy.rb
@@ -27,6 +27,9 @@ class EvaluationUserCapabilityPolicy < ApplicationPolicy
           .or(scope.where(user_id: user.id))
           .or(scope.where(manager_user_id: user.id))
       elsif user.hr_bp?
+        owned_user_ids = user.owned_calibration_sessions
+          .where(calibration_template_id: CalibrationTemplate.open_for_user_calibration_template_ids)
+          .collect { |cs| cs.calibration_session_users.collect(&:user_id) }.flatten
         hr_reviewed_user_ids = user.hr_reviewed_calibration_sessions
           .where(calibration_template_id: CalibrationTemplate.open_for_user_calibration_template_ids)
           .collect { |cs| cs.calibration_session_users.collect(&:user_id) }.flatten
@@ -37,7 +40,7 @@ class EvaluationUserCapabilityPolicy < ApplicationPolicy
         scope.where(dept_code: user.hrbp_user_managed_departments.pluck(:managed_dept_code))
           .or(scope.where(user_id: user.id))
           .or(scope.where(manager_user_id: user.id))
-          .or(scope.where(user_id: (hr_reviewed_user_ids + judge_user_ids).uniq))
+          .or(scope.where(user_id: (owned_user_ids + hr_reviewed_user_ids + judge_user_ids).uniq))
       else
         owned_user_ids = user.owned_calibration_sessions
           .where(calibration_template_id: CalibrationTemplate.open_for_user_calibration_template_ids)
