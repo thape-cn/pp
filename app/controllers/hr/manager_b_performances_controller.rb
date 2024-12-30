65c93df259aa17b002b9aad8f52ca742951b85f3
Eric Guo <eric.guocz@gmail.com>
2024-12-30 11:27:26 +0800

    Draft version of HR manager B performance review

---
+++ app/controllers/hr/manager_b_performances_controller.rb
@@ -0,0 +1,15 @@
+module HR
+  class ManagerBPerformancesController < BaseController
+    include ShowPerformance
+
+    private
+
+    def group_level
+      "manager_b"
+    end
+
+    def group(evaluation_user_capabilities)
+      manager_group_b(evaluation_user_capabilities)
+    end
+  end
+end
