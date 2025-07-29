7e4abf46f571508269f88cb6fc81c3761537d48f
Eric-Guo <eric.guocz@gmail.com>
2025-07-29 16:19:36 +0800

    Add HRBP access for archived_evaluation

--- app/policies/archived_evaluation_user_capability_policy.rb
+++ app/policies/archived_evaluation_user_capability_policy.rb
@@ -5,6 +5,8 @@ class ArchivedEvaluationUserCapabilityPolicy < ApplicationPolicy
         scope.all
       elsif user.hr_staff?
         scope.where(company: user.hr_user_managed_companies.pluck(:managed_company))
+      elsif user.hr_bp?
+        scope.where(dept_code: user.hrbp_user_managed_departments.pluck(:managed_dept_code))
       else
         scope.where(user_id: user.id)
           .or(scope.where(manager_user_id: user.id))
