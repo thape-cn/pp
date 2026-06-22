6772033967f0f09f721d3293fef6e5584f972e18
Eric-Guo <eric.guocz@gmail.com>
2026-06-22 15:43:35 +0800

    Rename evaluation supervisor roles to lead

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,7 @@ port ENV.fetch("PORT", 3000)
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments.
+# Run Solid Queue inside Puma for single-server deployments.
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
