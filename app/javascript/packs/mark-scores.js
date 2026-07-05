e82cdaef260460a69b2ea760b462f599f875cf79
Eric-Guo <eric.guocz@gmail.com>
2026-07-05 16:30:38 +0800

    Make mark scores table horizontally scrollable

--- app/javascript/packs/mark-scores.js
+++ app/javascript/packs/mark-scores.js
@@ -10,42 +10,34 @@ function mountWhenReady(callback) {
   }
 }
 
-const staff_node = document.getElementById('staff-mark')
-if (staff_node) {
-  const staff_root = createRoot(staff_node);
-  mountWhenReady(() => {
+mountWhenReady(() => {
+  const staff_node = document.getElementById('staff-mark')
+  if (staff_node) {
+    const staff_root = createRoot(staff_node);
     staff_root.render(<MarkScores group_level='staff' />);
-  });
-}
+  }
 
-const manager_a_node = document.getElementById('manager_a-mark');
-if (manager_a_node) {
-  const manager_root = createRoot(manager_a_node);
-  mountWhenReady(() => {
+  const manager_a_node = document.getElementById('manager_a-mark');
+  if (manager_a_node) {
+    const manager_root = createRoot(manager_a_node);
     manager_root.render(<MarkScores group_level='manager_a' />);
-  });
-}
+  }
 
-const manager_b_node = document.getElementById('manager_b-mark');
-if (manager_b_node) {
-  const manager_root = createRoot(manager_b_node);
-  mountWhenReady(() => {
+  const manager_b_node = document.getElementById('manager_b-mark');
+  if (manager_b_node) {
+    const manager_root = createRoot(manager_b_node);
     manager_root.render(<MarkScores group_level='manager_b' />);
-  });
-}
+  }
 
-const auxiliary_node = document.getElementById('auxiliary-mark');
-if (auxiliary_node) {
-  const auxiliary_root = createRoot(auxiliary_node);
-  mountWhenReady(() => {
+  const auxiliary_node = document.getElementById('auxiliary-mark');
+  if (auxiliary_node) {
+    const auxiliary_root = createRoot(auxiliary_node);
     auxiliary_root.render(<MarkScores group_level='auxiliary' />);
-  });
-}
+  }
 
-const supervisor_node = document.getElementById('supervisor-mark');
-if (supervisor_node) {
-  const supervisor_root = createRoot(supervisor_node);
-  mountWhenReady(() => {
+  const supervisor_node = document.getElementById('supervisor-mark');
+  if (supervisor_node) {
+    const supervisor_root = createRoot(supervisor_node);
     const mark_score_groups = JSON.parse(supervisor_node.getAttribute('data-mark-score-groups') || '[]');
     supervisor_root.render(
       <>
@@ -57,5 +49,5 @@ if (supervisor_node) {
         ))}
       </>
     );
-  });
-}
+  }
+});
